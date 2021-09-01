6001b96195c15900abd653f8158d72362e4927a6
Basant <56782336+basant7@users.noreply.github.com>
2021-09-01 11:47:52 +0530

    Binary Search Tree Implementation
    with operations such as insert , lookup and remove.

--- tree.js
+++ tree.js
@@ -72,6 +72,77 @@ class BinaryTree{
         }
         return false;
     }
+    
+    // to remove an element from the Binary Search Tree
+    remove(value){
+        if(this.root == null){
+            return false;
+        }
+        let currentNode = this.root;
+        let parentNode = null;
+        while(currentNode){
+            if(value < currentNode.value){
+                parentNode = currentNode;
+                currentNode = currentNode.left;
+            }
+            if(value > currentNode.value){
+                parentNode = currentNode;
+                currentNode = currentNode.right;
+            }
+            if(currentNode.value == value){
+                // option 1: no right child
+                if(currentNode.right == null){
+                    if(parentNode == null){
+                        this.root = currentNode.left;
+                    }else{
+                        if(currentNode.value < parentNode.value){
+                            parentNode.left = currentNode.left;
+                        }else if(currentNode.value > parentNode.value){
+                            parentNode.right = currentNode.left;
+                        }
+                    }
+                    // right child which doesn't have a left child
+                }else if(currentNode.right.left == null){
+                    if(parentNode == null){
+                        this.root = currentNode.left;
+                    }else{
+                        currentNode.right.left = currentNode.left;
+                        if(currentNode.value < parentNode.value){
+                            parentNode.left = currentNode.right;
+                        }else if(currentNode.value > parentNode.value){
+                            parentNode.right = currentNode.right;
+                        }
+                    }
+                    // right child that has a left child
+                }else{
+                    // find the right child's left most child
+                    let leftmost = currentNode.right.left;
+                    let leftmostparent = currentNode.right;
+                    while(leftmost.left != null){
+                        leftmostparent = leftmost;
+                        leftmost = leftmost.left;
+                    }
+
+                    // parent's left subtree is now leftmost's right subtree
+                    leftmostparent.left = leftmost.right;
+                    leftmost.left = currentNode.left;
+                    leftmost.right = currentNode.right;
+
+                    if(parentNode == null){
+                        this.root = leftmost;
+                    }else{
+                        if(currentNode.value < parentNode.value){
+                            parentNode.left = leftmost;
+                        }else if(currentNode.value > parentNode.value){
+                            parentNode.right = leftmost;
+                        }
+                    }
+                }
+                return true;
+            }
+        }
+    }
+    
 }
 
 const Tree = new BinaryTree();
@@ -81,6 +152,7 @@ Tree.insert(6);
 Tree.insert(20);
 Tree.insert(170);
 Tree.insert(15);
+Tree.remove(4);
 Tree.insert(1);
 console.log(Tree.lookup(1));
 console.log(JSON.stringify(traverse(Tree.root)))
