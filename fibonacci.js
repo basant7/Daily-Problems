0a4923e0350172818385eb5f7ecfbd54d2cc1a04
Basant <56782336+basant7@users.noreply.github.com>
2021-09-07 14:29:21 +0530

    Fibonacci series using multiple ways

--- fibonacci.js
+++ fibonacci.js
@@ -13,6 +13,17 @@ const fibonacci = (a,b) =>{
 
 console.log(fibonacci(0,1))
 
+// iterative way
+const fib = (n) => {
+    let arr = [0,1];
+    for(let i = 2; i < n+1; i++){
+        arr.push(arr[i-2] + arr[i-1]);
+    }
+    return arr[n];
+}
+
+console.log(fib(11));
+
 // fibonacci suing recursion
 const fibo = (n) =>{
     if(n<=1){
